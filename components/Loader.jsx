8813be1bc83d5122502a5c2ecc1e0913331adc5f
Juli-mc <julianmarincordoba19@gmail.com>
2022-10-13 14:11:02 -0500

    spinner with css

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -4,7 +4,9 @@ import React from "react";
 const Loader = () => {
   return (
     <div>
+        <div className="spinner">
       <img src="../src/img/Wedges-4s-307px.svg" alt="" srcset="" />
+        </div>
     </div>
   );
 };
